60f3614dd09f13cf02b73c69f9b25fa67f72f316
Anurodh_aka_idabora <80338513+idabora@users.noreply.github.com>
2021-10-12 20:36:24 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,8 +2,8 @@
 using namespce std;
 int main(){
   int a,b;
-  cout<<"enter two numbers";
+  cout<<"Enter two integers-";
   cin>>a>>b;
-  cout<<"the sum is "<<a+b;
+  cout<<"The sum is- "<<a+b;
   return 0;
 }
